86508b0a27da719628b4db6ace88a8ae622aa996
touka_aoi <toukaseisei@gmail.com>
2021-06-17 21:54:46 +0900

    by iput section

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 fn main() {
-    println!("Hello, world!");
-    println!("test");
-    println!("test");
+    //あいうえお
+    println!("test{}", 1 / 2);
+    //変数代入
+    let hoge; //よく見たらダイナミックだ...
+    hoge = 10;
+    println!("{}", hoge);
+    //型注釈
+    let hoge2: i32;
+    //生文字リテラル
+    println!(r"\\\\\\\\");
+    //ダブルクォーテーションリテラル
+    println!(r#""""""""""""""""""""""""a"bbb"#);
+    //リテラルをつける
+    let spam = 1_f32; //1のfloat32型
+
+    println!("{}", spam);
 }
