b0db6191425c4d885f1e311164c40da41696a3f9
touka_aoi <toukaseisei@gmail.com>
2021-06-17 22:04:54 +0900

    I learned input

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,8 @@
 fn main() {
-    //あいうえお
-    println!("test{}", 1 / 2);
-    //変数代入
-    let hoge; //よく見たらダイナミックだ...
-    hoge = 10;
-    println!("{}", hoge);
-    //型注釈
-    let hoge2: i32;
-    //生文字リテラル
-    println!(r"\\\\\\\\");
-    //ダブルクォーテーションリテラル
-    println!(r#""""""""""""""""""""""""a"bbb"#);
-    //リテラルをつける
-    let spam = 1_f32; //1のfloat32型
-
-    println!("{}", spam);
+    proconio::input! {
+        n: i32,
+        a: i32,
+        b: i32,
+    }
+    println!("{}", n - a + b);
 }
