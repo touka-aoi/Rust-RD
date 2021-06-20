8266d1db2f6315ef71a17909e6871a740569daa5
touka_aoi <toukaseisei@gmail.com>
2021-06-21 01:52:52 +0900

    assertest

---
+++ code/closure.rs
@@ -0,0 +1,6 @@
+fn main() {
+    //クロージャを試す
+    let add_once_V3 = |x| x + 1;
+
+    println!("{}", add_once_V3(3));
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
 use proconio::input;
 
 fn main() {
-    //方針
-    //偶数でYが4X以下かつ2X以上ならいける
     input! {
-        x: i32,
-        y: i32,
+        x: i32;
     }
-    if y % 2 == 0 && 2 * x <= y && y <= 4 * x {
-        println!("Yes");
-    } else {
-        println!("No");
-    }
+    let r = x % 10;
+    //チェックしてあげる
+    assert!(0 <= r && r < 10);
+    println!("あまりは {}", r);
 }
